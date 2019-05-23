add86f6088949a1cf61643ebfab6550fdfa9983e
peter <peter@btgjkd.geo.uni-bayreuth.de>
2019-05-23 16:54:21 +0200

    Starting precip reduction script

--- R_scripts/precipitation reduction calculations.R
+++ R_scripts/precipitation reduction calculations.R
@@ -4,6 +4,7 @@
 
 path <- 'C:/Users/peter/Dropbox/IDE Meeting_May2019'
 
-precip <- read.csv(file.path(path,'Daily precip first two years 20190523.csv'))
+precip <- read.csv(file.path(path,'IDE Site Info/GHCN_daily_precip_preliminary20190522.csv'))
 
-
+siteDrt <- read.csv(file.path(path,'IDE Site Info/Sites_Loc_DrtTrt.csv'))
+siteDate <- read.csv(file.path(path,''))
