260ee89e70d4f4f0bf4c69315d9edec375d21321
Martin Holdrege <martinholdrege@gmail.com>
2019-05-24 07:30:24 -0600

    percentiles not calculating for all sites yet.

--- R_scripts/calculate_cdf.R
+++ R_scripts/calculate_cdf.R
@@ -1,10 +1,14 @@
 #####################################################################
 ### script for estimating cumulative distribution functions by site ##
+### and calculating percentiles of annual precipitation of treatments#
+### during experiment years. 
 #######################################################################
 
 # script started 5/23/19
 
 library(tidyverse)
+library(spatstat)
+
 # path to data folders
 path <- 'C:/Users/grad/Dropbox/IDE Meeting_May2019'
 
@@ -27,28 +31,76 @@ precip1 <- lapply(file_names1, function(file_name){
     select(site_code, year, totalPRE)
 })
 
-
 precip2 <- lapply(file_names2, function(file_name){
   read.csv(file.path(precip_folder2, file_name), as.is = TRUE) %>% 
     select(site_code, year, totalPRE)
 })
 
-
 precip3 <- c(precip1, precip2) # list of dataframes from both sites
 
-# data check
-sapply(precip3, function(x){
-  length(unique(x$site_code) != 1)
-}) %>% sum() # should be zero
-
+# extracting site codes from each file
 site_codes <- sapply(precip3, function(df){
-  sc <- df$site_code[[1]]
-  print(sc)
+  # check: all should have one unique site code
   stopifnot(length(unique(df$site_code)) == 1)
+  
+  sc <- df$site_code[[1]]
   
   sc
 })
 
-# ppt_df1 <- bind_rows(precip3)
+# nameing list elements
+names(precip3) <- site_codes
 
 
+#   -----------------------------------------------------------------------
+
+# compute probability density function for each site, based on last 50 years
+
+# density functions
+# this is like a smoothed histogram
+density <- lapply(precip3, function(df){
+  ppt <- df %>% 
+    filter(year > 1964) %>% 
+    .$totalPRE
+  d <- density(ppt)
+  d
+})
+
+par(mfrow = c(2, 2))
+walk2(density, site_codes, function(x, y){
+  plot(x, main = y)
+})
+
+# compute cumulative density function -------------------------------------
+
+# this ~ takes the area under the curve of the density function
+# a smoothed version of the emperical cumulative density function (as one would get with ecdf())
+cdf1 <- lapply(density, function(x){
+  cdf <- CDF(x)
+  cdf
+})
+
+
+# observed precipitation by site/year/trmt --------------------------------
+
+site_ppt <- read.csv(file.path(path, "IDE Site Info\\Site-year precipitation by treatment.csv"), as.is = TRUE)
+
+site_ppt2 <- site_ppt
+
+
+# calculating percentiles of ap given annual precip -----------------------
+
+site_ppt2$percentile <- NA
+# "scruzh.us" still need
+for (i in 1:nrow(site_ppt2)){
+  site_code <- site_ppt2$site_code[i]
+  ppt <- site_ppt2$ppt[i] # precip for the site/year
+  print(site_code)
+  # percentile based on cdf for the site
+  site_ppt2$percentile[i] <- cdf1[[site_code]](ppt)
+}
+
+site_ppt2 %>% 
+  filter(!(site_code %in% site_codes)) %>% 
+  .$site_code %>% 
+  unique()
