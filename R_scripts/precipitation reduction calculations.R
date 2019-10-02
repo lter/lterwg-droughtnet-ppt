841794f2aaeba344c7a5aeb741147ccda2ba1fec
MartinHoldrege <martinholdrege@gmail.com>
2019-10-02 12:28:35 -0600

    work in progress

--- R_scripts/precipitation reduction calculations.R
+++ R_scripts/precipitation reduction calculations.R
@@ -26,14 +26,14 @@ precip <- read.csv(file.path(path_oct,'data/precip/GHCN_daily_precip_2019-10-01.
 precip_submitted <- read.csv(file.path(path_oct,'data/precip/submitted_daily_weather_2019-10-01.csv'),
                    as.is = TRUE)
 
-
+# may not need this?
 siteDrt <- read.csv(file.path(path,'IDE Site Info/Sites_Loc_DrtTrt.csv'))
 
-siteBio <- read.csv(
+siteBio_A <- read.csv(
   file.path(path_oct, "Full biomass\\Full_Biomass-SurveyResults_10-01-2019.csv"),
   as.is = TRUE, na.strings = c("NULL"))
 
-siteBio <- siteBio[!siteBio$trt %in% c('NPK','NPK_Drought'),]
+siteBio_A <- siteBio_A[!siteBio_A$trt %in% c('NPK','NPK_Drought'),]
 
 
 # combine precip ----------------------------------------------------------
@@ -60,60 +60,88 @@ precip3 <- bind_rows(precip2, precip_submitted2) %>%
 names(precip3)
 
 
-# site level information --------------------------------------------------
-
-min <- function(x){
-  # return first element (assuming all elements are the same)
-  stopifnot(
-    is.vector(x),
-    length(unique(x)) == 1
-  )
-  if(length(unique(x)) != 1) {
-    warning("more than 1 unique value")
-  }
-  
-  min(x, na.rm = TRUE)
-}
-
-lu <- function(x) length(unique(x))
-
-# sites with more than one start treatment year etc
-# STOP: NEED TO FIGURE OUT HOW TO DEAL WITH SITES WITH MULTIPLE START DATES
-siteBio %>% 
-  group_by(site_code, first_treatment_date) %>% 
-  summarize_at(vars(first_treatment_year, 
-                    IfNot365.WhenShelterSet, IfNot365.WhenShelterRemove),
-               .funs = lu) %>% View()
-
-sites1 <- siteBio %>% 
-  group_by(site_code, first_treatment_date) %>% 
-  summarize_at(vars(first_treatment_year, 
-                    IfNot365.WhenShelterSet, IfNot365.WhenShelterRemove),
-               .funs = lu) 
-
-siteBio %>% 
-  filter(site_code == "bayrdrt.de") %>% View
-
-siteBio %>% 
-  filter(site_code == "brandjberg.dk") 
-
-## filter out sites without precip data
-siteBio <- siteBio[siteBio$site_code %in% precip$site_code,]
-
+# siteBio--process dates --------------------------------------------------
 
 ### convert site biomass harvest date to POSIX (date) format
 
 ### remove weird \n in treatment dates
 
-siteBio$first_treatment_date <- gsub('\n','',siteBio$first_treatment_date)
-siteBio$biomass_date <- gsub('\n','',siteBio$biomass_date)
+siteBio_A$first_treatment_date <- gsub('\n','',siteBio_A$first_treatment_date)
+siteBio_A$biomass_date <- gsub('\n','',siteBio_A$biomass_date)
+
 
 ### convert to POSIX (date) format
-siteBio$bioDat <- as.POSIXct(siteBio$biomass_date,format = '%m/%d/%Y')
-siteBio$trtDat <- as.POSIXct(siteBio$first_treatment_date,format = '%m/%d/%Y')
+siteBio_A$bioDat <- as.POSIXct(siteBio_A$biomass_date,format = '%m/%d/%Y')
+siteBio_A$trtDat <- as.POSIXct(siteBio_A$first_treatment_date,format = '%m/%d/%Y')
 #### some sites are randomly in other format
-siteBio[is.na(siteBio$trtDat),]$trtDat <- as.POSIXct(siteBio[is.na(siteBio$trtDat),]$first_treatment_date,format = '%Y-%m-%d')
+siteBio_A[is.na(siteBio_A$trtDat),]$trtDat <- as.POSIXct(siteBio_A[is.na(siteBio_A$trtDat),]$first_treatment_date,format = '%Y-%m-%d')
 
+sum(is.na(siteBio_A$trtDat))
+sum(is.na(siteBio_A$bioDat)) #some didn't parse
+
+
+# STOP: some didn't parse, because just 2017 was given as date--needs to be resolved
+siteBio_A[is.na(siteBio_A$bioDat), ]
+
+# site level information --------------------------------------------------
+siteBio_B <- siteBio_A
+
+
+lu <- function(x) length(unique(x)) # length (number of) unique values
+
+first <- function(x){
+  stopifnot(
+    is.vector(x),
+    length(unique(x)) == 1
+  )
+  x[1]
+}
+
+# sites with more than one start treatment year/date etc:
+
+# STOP: NEED TO FIGURE OUT HOW TO DEAL WITH SITES WITH MULTIPLE START DATES
+siteBio_B %>% 
+  group_by(site_code) %>% 
+  summarize_at(vars(trtDat, first_treatment_year, 
+                    IfNot365.WhenShelterSet, IfNot365.WhenShelterRemove),
+               .funs = lu) %>% 
+  filter(trtDat >1  | first_treatment_year > 1 | IfNot365.WhenShelterSet > 1 |
+         IfNot365.WhenShelterRemove > 1)
+
+siteBio_B %>% 
+  filter(site_code == "bayrdrt.de")
+
+siteBio_B %>% 
+  filter(site_code == "brandjberg.dk") 
+
+# STOP: here I am taking the min first_treatment date--should more carefully
+# consider fi this is a good decision
+sites1 <- siteBio_B %>% 
+  group_by(site_code) %>% 
+  summarize(first_treatment_year = min(first_treatment_year, na.rm = TRUE), 
+            trtDat = min(trtDat), 
+            IfNot365.WhenShelterSet = first(IfNot365.WhenShelterSet),
+            IfNot365.WhenShelterRemove = first(IfNot365.WhenShelterRemove),
+            X365day.trt = first(X365day.trt))
+
+
+
+
+# trying different method--joining with precip data -----------------------
+names(sites1)
+precip4 <- precip3 %>% 
+  left_join(sites1, by = "site_code") %>% 
+  mutate(n_treat_days = difftime(date, trtDat, units="days"))
+
+
+str(precip4)
+
+
+
+#running until this point....
+
+# ## filter out sites without precip data
+# siteBio <- siteBio[siteBio$site_code %in% precip$site_code,]
 #### Rewrite number of treatment years based on n_treat_days for early years
 
 siteBio$nTrtYr <- siteBio$n_treat_years
