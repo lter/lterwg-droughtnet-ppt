db1940a16be1bb4dfdb40dc0b4b068710ba32964
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2026-03-25 14:43:58 -0600

    update dates

--- R_scripts/05_precipitation reduction calculations.R
+++ R_scripts/05_precipitation reduction calculations.R
@@ -28,9 +28,9 @@ path_ms <-  file.path(path, "IDE MS_Single year extreme")
 # biomass harvest will be calculated
 
 
-days_before <-    1460 #120  #365 #730  #1095  # 
+days_before <-   1460 #120  #365 #730  # 1095  # 
 
-date_string <- "2025-10-20" # for use in output file names
+date_string <- "2026-03-25" # for use in output file names
 
 if (days_before%%365 == 0) {
   window <- 365
